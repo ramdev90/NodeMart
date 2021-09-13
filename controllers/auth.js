a34fbcafe5159bed2d8944cf628de94f8fd4b985
Ramdev <ramdevrathod90@gmail.com>
2021-09-13 15:04:29 +0530

    error fixed

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator')
 
 const User = require('../models/user');
 const user = require('../models/user');
@@ -76,38 +77,39 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422)
+      .render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        errorMessage: errors.array()[0].msg
+      });
+  }
   User.findOne({ email: email })
-    .then(userDoc => {
-      if (userDoc) {
-        req.flash('error', 'E-Mail exists already, please pick a different one.');
-        return res.redirect('/signup');
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then(hashedPassword => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] }
-          });
-          return user.save();
-        })
-        .then(result => {
-          res.redirect('/login');
-          return trasportor.sendMail({
-            to: email,
-            from: 'ramdevrathod900@gmail.com',
-            subject: 'just an sub example',
-            html: '<h1> success compete </h1>'
-          });
-        }).then(result => {
-          console.log('done');
-        })
-        .catch(err => console.log(err));
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] }
+      });
+      return user.save();
     })
-    .catch(err => {
-      console.log(err);
-    });
+    .then(result => {
+      res.redirect('/login');
+      return trasportor.sendMail({
+        to: email,
+        from: 'ramdevrathod900@gmail.com',
+        subject: 'just an sub example',
+        html: '<h1> success compete </h1>'
+      });
+    }).then(result => {
+      console.log('done');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postLogout = (req, res, next) => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -10,7 +12,33 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post('/signup',
+    [
+        check('email')
+            .isEmail()
+            .withMessage('Please enter a valid Email.')
+            .custom((value, { req }) => {
+                // if (value === 'test@gmail.com') {
+                //     throw new Error('This email address is forbidder');
+                // }
+                // return true
+                return User.findOne({ email: value })
+                    .then(userDoc => {
+                        if (userDoc) {
+                            return Promise.reject('E-mail exist already, please pick a different one.');
+                        }
+                    });
+            }),
+        body('password', 'Please enter a password with only number and text at lease 5 characters.')
+            .isLength({ min: 5 })
+            .isAlphanumeric(),
+        body('confirmPasswrod').custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Password have to match!');
+            }
+            return true;
+        })
+    ], authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
