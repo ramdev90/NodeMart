b844e188fd4d7d1aea14b8c256ffc70d341933d1
Ramdev <ramdevrathod90@gmail.com>
2022-08-19 20:05:50 +0530

    process env set

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
-    secret: 'my secret',
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
     store: store
